a9b3149c723bfaafcfeae48e124727f3b1f30b8b
BorgField <147969784+BorgField@users.noreply.github.com>
2025-09-02 17:13:10 +0800

    删除额外模组

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/expansion/mergeManagers/charmodifier/expansion/MagicBuff.java
+++
@@ -1,6 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.expansion.mergeManagers.charmodifier.expansion;
-
-import com.shatteredpixel.shatteredpixeldungeon.expansion.mergeManagers.charmodifier.ModifierTemplate;
-
-public class MagicBuff extends ModifierTemplate {
-}
